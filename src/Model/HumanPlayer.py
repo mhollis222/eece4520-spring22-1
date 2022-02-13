e9b70c85204b9dc39d9d92261de61eb624314731
Santiago Pinzon <pinzon.s@northeastern.edu>
2022-02-13 15:19:20 -0500

    Quick fixes to the player class

--- src/Model/HumanPlayer.py
+++ src/Model/HumanPlayer.py
@@ -8,12 +8,19 @@ class HumanPlayer(AbstractPlayer):
         valid = False
         while not valid:
             x = input(str(self) + ' pick the desired x coordinate, or enter \'pass\' to skip:\n')
+            valid = True
             if x.lower() == 'pass':
+                print(str(self) + ' skipped their turn!')
                 return Move(0, 0, True)
-            valid = int(x) > 0
+            if int(x) < 0:
+                valid = False
+                print('Coordinates must be > 0')
         valid = False
         while not valid:
             y = input(str(self) + ' pick the desired y coordinate:\n')
-            valid = int(y) > 0
+            valid = True
+            if int(y) < 0:
+                valid = False
+                print('Coordinates must be > 0')
 
         return Move(int(x), int(y))
