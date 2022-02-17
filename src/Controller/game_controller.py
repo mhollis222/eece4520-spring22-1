a0326e3c38623e7b78284f354d028d928b7528d4
Shriya-Bedi <63390035+Shriya-Bedi@users.noreply.github.com>
2022-02-17 01:23:46 -0500

    added second counter to track end of game

--- src/Controller/game_controller.py
+++ src/Controller/game_controller.py
@@ -2,7 +2,6 @@ from Model.Game import Game, Cell
 from Model.Move import Move
 from View.abstract_view import AbstractView
 
-
 class GameController:
 
     def __init__(self, model: Game, view: AbstractView):
@@ -12,10 +11,10 @@ class GameController:
     def play_game(self):
         self.model.start()
 
+        end = 0
         while self.model.running:
             counter = 0
-
-            while counter <= 1:
+            while counter <= 1 and end < 2:
 
                 player = self.model.order[counter]
 
@@ -46,3 +45,7 @@ class GameController:
                     counter = counter + 1
                     if not counter > 1:
                         self.view.display_player_skipped(self.model.order[counter])
+                        end = end + 1
+                        if end == 2:
+                            print("Game Over")
+                            self.view.display_winner(counter)
