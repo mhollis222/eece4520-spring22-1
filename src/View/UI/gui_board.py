aa077412768c087e3007c7e176bf260692c6367a
icyoutoo <66316658+icyoutoo@users.noreply.github.com>
2022-03-01 17:43:22 -0500

    Testing Branch

--- src/View/UI/gui_board.py
+++ src/View/UI/gui_board.py
@@ -1,1 +1,13 @@
-import
+from View.abstract_view import AbstractView
+from Model.game import Game
+from Model.abstract_player import AbstractPlayer
+import tkinter as tk
+
+class gui_board(AbstractView):
+
+    def __init__(self, model: Game):
+        super().__init__(model)
+        self.model = model
+
+    root = tk.Tk()
+    root.mainloop()
