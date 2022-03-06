391a3fe110f9920a6f479bf99f01eb0b854cceff
icyoutoo <66316658+icyoutoo@users.noreply.github.com>
2022-03-06 16:14:31 -0500

    Updated what I have so far for GUI board

---
+++ src/View/UI/current_player_status.py
@@ -0,0 +1,23 @@
+class CurrentPlayerStatus:
+    def __init__(self, name: str, score1: str, score2: str):
+        self.name = name
+        self.score1 = score1
+        self.score2 = score2
+
+    def current_player(player):
+        name = player
+
+    def get_player(self):
+        return self.name
+
+    def current_score1(points):
+        score1 = points
+
+    def get_score1(self):
+        return self.score1
+
+    def current_score2(points):
+        score2 = points
+
+    def get_score2(self):
+        return self.score2
