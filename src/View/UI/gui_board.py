ce4c5b9df15892d8642b8dbb46efde874c08d9d8
Santiago Pinzon <pinzon.s@northeastern.edu>
2022-03-07 23:11:07 -0500

    Hopefully this fixes most things

--- src/View/UI/gui_board.py
+++ src/View/UI/gui_board.py
@@ -26,27 +26,27 @@ class GuiBoard(AbstractView):
         self.empty_color = 'green'
         self.root = tk.Tk()
         self.root.geometry("1000x1000")
-        self.root.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], minsize=50, weight=1)
-        self.root.columnconfigure([0, 1, 2, 3, 4, 5, 6, 7], minsize=50, weight=1)
+        self.root.rowconfigure(list(range(len(self.model.get_board()) + 3)), minsize=50, weight=1)
+        self.root.columnconfigure(list(range(len(self.model.get_board()[0]))), minsize=50, weight=1)
+        self.board_frame = tk.Frame(self.root, width=600, height=600)
+        self.board_frame.grid(row=0, column=0, columnspan=len(self.model.get_board()[0]), rowspan=len(self.model.get_board()))
 
     def display_board(self, valid_moves: list):
-
+        self.board_frame.grid_forget()
         board_view = self.model.get_board()
-        self.root.board_frame = tk.Frame(self.root, width=600, height=600)
-        self.root.board_frame.grid(row=0, column=0, columnspan=8, rowspan=8)
 
-        for x in range(8):
-            self.root.board_frame.rowconfigure(x, minsize=75)
-            self.root.board_frame.columnconfigure(x, minsize=75)
+
+        for x in range(len(board_view)):
+            self.board_frame.rowconfigure(x, minsize=75)
+            self.board_frame.columnconfigure(x, minsize=75)
 
         for i, x in enumerate(board_view):
-            for y in range(8):
+            for y in range(len(board_view[0])):
                 if (y, i) in valid_moves:
-                    button = ReversiButton(i, y, '0_0', callback=self.controller.advance, state=tk.ACTIVE, color=self.empty_color)
+                    button = ReversiButton(i, y, '0_0', callback=self.controller.advance, state=tk.ACTIVE, color=self.valid_color)
                     button.grid(row=i, column=y, sticky='nsew')
-
                 elif x[y].value == 0:
-                    button = ReversiButton(i, y, 'empty', callback=self.controller.advance, state=tk.DISABLED, color=self.valid_color)
+                    button = ReversiButton(i, y, 'empty', callback=self.controller.advance, state=tk.DISABLED, color=self.empty_color)
                     button.grid(row=i, column=y, sticky='nsew')
                 elif x[y].value == 1:
                     button = ReversiButton(i, y, 'p1', callback=self.controller.advance, state=tk.DISABLED, color=self.p1_color)
