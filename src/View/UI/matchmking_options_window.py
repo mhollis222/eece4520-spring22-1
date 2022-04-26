7713ccb10949d9b5dbe8516ed010bfdc546c6127
snazaire712 <snazaire712@hotmail.com>
2022-04-26 16:35:15 -0400

    Updated online and matchmaking window

---
+++ src/View/UI/matchmking_options_window.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from Controller.client import ReversiClient
+from Controller.message import ReversiMessage as msg
+from tkinter import messagebox
+from settings_window import SettingsWindow
+import configparser
+
+from pathlib import Path
+path_parent = Path(__file__).resolve().parents[3]
+preference_path = path_parent.joinpath('preferences.ini').as_posix()
+
+
+class OnlineWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.title('Online Challenge Page')
+        self.geometry("2000x2000")
+        self.rowconfigure([0, 1, 2, 3], minsize=50, weight=1)
+        self.columnconfigure([0, 1, 2], minsize=50, weight=1)
+        self.configure(bg='green')
+        self.config = configparser.ConfigParser(comment_prefixes='/', allow_no_value=True)
+        self.config.read(preference_path)
+        self.client = ReversiClient()
+        self.player = None
+
+        # back button
+        self.back_button = tk.Button(self, text='Back', width=10, height=2, font=("Arial", 12),
+                                     activebackground='green', bg='green', fg='white', relief='flat',
+                                     command=self.open_root)
+        self.back_button.grid(row=0, column=0, padx=0, sticky='nw')
+        # title
+        self.label1 = tk.Label(self, text='Challenge a User Online', fg='white', font=("Arial", 30, "bold"), bg='green')
+        self.label1.grid(row=0, columnspan=3, sticky=tk.S)
+        # list of online players
+        self.players = tuple(self.client.send_request(msg('get_players', [])))
+        self.players_var = tk.StringVar(value=self.players)
+        self.listbox = tk.Listbox(listvariable=self.players_var)
+        self.listbox.grid(row=1, columnspan=3, sticky=tk.S)
+        # chosen player
+        self.chosen_label = tk.Label(self, text='Opponent:', bg='green', font=("Arial", 20), fg='white')
+        self.chosen_label.grid(row=2, column=0, sticky=tk.E, padx=5, pady=5)
+        self.chosen_entry = tk.Entry(self, font=("Arial", 20))
+        self.chosen_entry.grid(row=2, column=1, sticky=tk.W, padx=15, pady=5)
+        # self.listbox.bind('<<ListboxSelect>>', self.player_selected)
+        # challenge button
+        self.challenge_button = tk.Button(self, text='Challenge', width=30, height=2, font=("Arial", 15),
+                                       command=self.challenge)
+        self.challenge_button.grid(row=3, column=0, columnspan=3, padx=5, pady=5, sticky=tk.N)
+
+    def open_root(self):
+        """Naviagtes to the login page"""
+        self.destroy()
+        self.master.deiconify()  # show the root window
+
+    def challenge(self):
+        if self.player is None:
+            messagebox.showerror("", "You must select a player to challenge")
+        else:
+            self.config['User']['opponent'] = self.chosen_entry.get()
+            self.save_preferences()
+            settings_options_win = SettingsWindow(self)
+            settings_options_win.focus_force()
+            self.withdraw()
+
+
