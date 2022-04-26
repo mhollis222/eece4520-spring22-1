23eba3ea404cf3e3c5c376be7bc87153c97e007e
snazaire712 <snazaire712@hotmail.com>
2022-04-26 10:07:14 -0400

    atered text

--- src/View/UI/matchmaking_error_window.py
+++ src/View/UI/matchmaking_error_window.py
@@ -13,13 +13,17 @@ class MatchmakingErrorWindow(tk.Toplevel):
 
         # title
         self.title = tk.Label(self,
-                              text='Unfortunately, we were unable to connect you to someone. Please try again later.',
-                              fg='white', font=("Arial", 55, "bold"), bg='green')
-        self.title.grid(row=1, columnspan=2, sticky=tk.S)
+                              text='Unable to currently matchmake.',
+                              fg='white', font=("Arial", 45, "bold"), bg='green')
+        self.title.grid(row=1, columnspan=2, sticky=tk.N)
+        self.title2 = tk.Label(self,
+                               text='Please try again later.',
+                               fg='white', font=("Arial", 45, "bold"), bg='green')
+        self.title2.grid(row=1, columnspan=2, sticky=tk.S)
         # cancel button
         self.cancel_button = tk.Button(self, text='OK', width=30, height=2, font=("Arial", 15),
                                        command=self.cancel)
-        self.cancel_button.grid(row=3, columnspan=2, padx=5, pady=5)
+        self.cancel_button.grid(row=3, columnspan=2, padx=5, pady=5, sticky=tk.S)
 
     def cancel(self):
         self.withdraw()
